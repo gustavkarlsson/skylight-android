cef9a682694f9f71d6cb067f8eafad047cd0fc71
Gustav Karlsson <gustav.karlsson@gmail.com>
2017-01-19 20:22:52 +0100

    Prepared for debug build variant

--- app/src/main/java/se/gustavkarlsson/aurora_notifier/android/gui/activities/MainActivity.java
+++ app/src/main/java/se/gustavkarlsson/aurora_notifier/android/gui/activities/MainActivity.java
@@ -59,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
 		int id = item.getItemId();
 
 		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
+		if (id == R.id.action_debug) {
 			return true;
 		}
 
