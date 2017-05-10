d05e7da77fb9fd4e8cc0857c506e42c9dee0337c
Gustav Karlsson <gustav.karlsson@gmail.com>
2017-05-11 00:48:13 +0200

    Added uncaught exception handler

--- app/src/main/java/se/gustavkarlsson/aurora_notifier/android/AuroraNotifier.java
+++ app/src/main/java/se/gustavkarlsson/aurora_notifier/android/AuroraNotifier.java
@@ -4,6 +4,9 @@ import android.app.Application;
 import android.content.Context;
 import android.util.Log;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 import se.gustavkarlsson.aurora_notifier.android.background.UpdateScheduler;
 import se.gustavkarlsson.aurora_notifier.android.dagger.components.ApplicationComponent;
 import se.gustavkarlsson.aurora_notifier.android.dagger.components.DaggerApplicationComponent;
@@ -16,6 +19,7 @@ public class AuroraNotifier extends Application {
 
 	@Override
 	public void onCreate() {
+		Thread.setDefaultUncaughtExceptionHandler(new LoggingUncaughtExceptionHandler());
 		Log.v(TAG, "onCreate");
 		super.onCreate();
 		this.applicationComponent = DaggerApplicationComponent.builder()
@@ -26,6 +30,18 @@ public class AuroraNotifier extends Application {
 
 	public static ApplicationComponent getApplicationComponent(Context context) {
 		return ((AuroraNotifier) context.getApplicationContext()).applicationComponent;
+	}
+
+	private static class LoggingUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
+		@Override
+		public void uncaughtException(Thread thread, Throwable throwable) {
+			StringWriter sw = new StringWriter();
+			PrintWriter pw = new PrintWriter(sw);
+			throwable.printStackTrace(pw);
+			String stackTrace = sw.toString();
+			Log.e(TAG, "The application has crashed!\n" + stackTrace);
+			System.exit(-1);
+		}
 	}
 
 }
