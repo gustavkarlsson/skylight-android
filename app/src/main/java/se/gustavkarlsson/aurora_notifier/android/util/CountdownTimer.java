5e6338bde3a79f92ba54ce9b7ba1fd784cea197a
Gustav Karlsson <gustav.karlsson@gmail.com>
2017-05-30 21:31:34 +0200

    Added tests for CountdownTimer

--- app/src/main/java/se/gustavkarlsson/aurora_notifier/android/util/CountdownTimer.java
+++ app/src/main/java/se/gustavkarlsson/aurora_notifier/android/util/CountdownTimer.java
@@ -1,18 +1,26 @@
 package se.gustavkarlsson.aurora_notifier.android.util;
 
+import org.threeten.bp.Clock;
+
 public class CountdownTimer {
 	private final long expiryTimeMillis;
+	private final Clock clock;
 
-	private CountdownTimer(long expiryTimeMillis) {
-		this.expiryTimeMillis = expiryTimeMillis;
+	private CountdownTimer(long expiryTimeMillis, Clock clock) {
+		this.expiryTimeMillis = clock.millis() + expiryTimeMillis;
+		this.clock = clock;
 	}
 
 	public static CountdownTimer start(long durationMillis) {
-		return new CountdownTimer(System.currentTimeMillis() + durationMillis);
+		return start(durationMillis, Clock.systemUTC());
+	}
+
+	public static CountdownTimer start(long durationMillis, Clock clock) {
+		return new CountdownTimer(durationMillis, clock);
 	}
 
 	public long getRemainingTimeMillis() {
-		long remainingTIme = expiryTimeMillis - System.currentTimeMillis();
+		long remainingTIme = expiryTimeMillis - clock.millis();
 		return Math.max(remainingTIme, 0);
 	}
 
