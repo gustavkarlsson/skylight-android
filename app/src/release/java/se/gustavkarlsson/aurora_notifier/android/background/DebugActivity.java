8e020ebf2ed7c9a26e6161449d4d153bfc83ca90
Gustav Karlsson <gustav.karlsson@gmail.com>
2017-05-31 10:40:54 +0200

    Removed DebugActivity from release sources

--- app/src/release/java/se/gustavkarlsson/aurora_notifier/android/background/DebugActivity.java
+++
@@ -1,7 +0,0 @@
-package se.gustavkarlsson.aurora_notifier.android.background;
-
-import android.support.v7.app.AppCompatActivity;
-
-public class DebugActivity extends AppCompatActivity {
-	// Dummy class necessary for compiling. Will never be run
-}
