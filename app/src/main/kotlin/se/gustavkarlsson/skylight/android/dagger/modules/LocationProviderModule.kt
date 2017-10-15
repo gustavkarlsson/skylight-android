cb9187af2a8b8e2d1956e77aab7620949c3bcace
Gustav Karlsson <gustav.karlsson@gmail.com>
2017-10-15 19:24:48 +0200

    Improved location provider

--- app/src/main/kotlin/se/gustavkarlsson/skylight/android/dagger/modules/LocationProviderModule.kt
+++ app/src/main/kotlin/se/gustavkarlsson/skylight/android/dagger/modules/LocationProviderModule.kt
@@ -1,13 +1,11 @@
 package se.gustavkarlsson.skylight.android.dagger.modules
 
 import android.content.Context
-import com.google.android.gms.common.api.GoogleApiClient
-import com.google.android.gms.location.LocationServices
 import dagger.Module
 import dagger.Provides
 import dagger.Reusable
 import se.gustavkarlsson.skylight.android.services.providers.LocationProvider
-import se.gustavkarlsson.skylight.android.services_impl.providers.location.GoogleLocationProvider
+import se.gustavkarlsson.skylight.android.services_impl.providers.location.AndroidLocationProvider
 
 @Module
 class LocationProviderModule {
@@ -15,16 +13,6 @@ class LocationProviderModule {
     @Provides
     @Reusable
     fun provideLocationProvider(
-            googleApiClient: GoogleApiClient
-    ): LocationProvider = GoogleLocationProvider(googleApiClient)
-
-    @Provides
-    @Reusable
-    fun provideGoogleApiLocationClient(
             context: Context
-    ): GoogleApiClient {
-        return GoogleApiClient.Builder(context)
-                .addApi(LocationServices.API)
-                .build()
-    }
+    ): LocationProvider = AndroidLocationProvider(context)
 }
---
+++ app/src/main/kotlin/se/gustavkarlsson/skylight/android/services_impl/providers/location/AndroidLocationProvider.kt
@@ -0,0 +1,44 @@
+package se.gustavkarlsson.skylight.android.services_impl.providers.location
+
+import android.content.Context
+import com.google.android.gms.location.LocationServices
+import dagger.Reusable
+import kotlinx.coroutines.experimental.delay
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.debug
+import org.jetbrains.anko.warn
+import se.gustavkarlsson.skylight.android.R
+import se.gustavkarlsson.skylight.android.services.Location
+import se.gustavkarlsson.skylight.android.services.providers.LocationProvider
+import se.gustavkarlsson.skylight.android.util.UserFriendlyException
+import javax.inject.Inject
+
+@Reusable
+class AndroidLocationProvider
+@Inject
+constructor(
+	private val context: Context
+) : LocationProvider, AnkoLogger {
+
+    suspend override fun getLocation(): Location {
+        try {
+            debug("Getting location...")
+            val getLocationTask = LocationServices.getFusedLocationProviderClient(context).lastLocation
+			while (!getLocationTask.isComplete) {
+				delay(50)
+			}
+			if (!getLocationTask.isSuccessful) {
+				throw UserFriendlyException(R.string.error_could_not_determine_location, getLocationTask.exception!!)
+			}
+			val location = getLocationTask.result
+            debug("Location is: $location")
+            if (location == null) {
+                throw UserFriendlyException(R.string.error_could_not_determine_location, "Location API returned null")
+            }
+            return Location(location.latitude, location.longitude)
+        } catch (e: SecurityException) {
+            warn("Location permission missing", e)
+            throw UserFriendlyException(R.string.error_location_permission_missing, e)
+        }
+    }
+}
