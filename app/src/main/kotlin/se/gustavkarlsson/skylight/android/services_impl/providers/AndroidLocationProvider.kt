ba45e22c8899e2274f10d71a107e44b52ecf6125
Gustav Karlsson <gustav.karlsson@gmail.com>
2017-10-17 22:33:38 +0200

    Cleaned up AndroidLocationProvider a bit

--- app/src/main/kotlin/se/gustavkarlsson/skylight/android/services_impl/providers/AndroidLocationProvider.kt
+++ app/src/main/kotlin/se/gustavkarlsson/skylight/android/services_impl/providers/AndroidLocationProvider.kt
@@ -1,6 +1,7 @@
 package se.gustavkarlsson.skylight.android.services_impl.providers
 
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.tasks.Task
 import dagger.Reusable
 import kotlinx.coroutines.experimental.delay
 import org.jetbrains.anko.AnkoLogger
@@ -23,21 +24,39 @@ constructor(
         try {
             debug("Getting location...")
 			val getLocationTask = fusedLocationProviderClient.lastLocation
-			while (!getLocationTask.isComplete) {
-				delay(50)
-			}
-			if (!getLocationTask.isSuccessful) {
-				throw UserFriendlyException(R.string.error_could_not_determine_location, getLocationTask.exception!!)
-			}
+			waitForLocation(getLocationTask)
+			checkForErrorsError(getLocationTask)
 			val location = getLocationTask.result
             debug("Location is: $location")
-            if (location == null) {
-                throw UserFriendlyException(R.string.error_could_not_determine_location, "Location API returned null")
-            }
             return Location(location.latitude, location.longitude)
         } catch (e: SecurityException) {
             warn("Location permission missing", e)
             throw UserFriendlyException(R.string.error_location_permission_missing, e)
         }
     }
+
+	private suspend fun waitForLocation(getLocationTask: Task<android.location.Location>) {
+		var timeRemaining = timeoutMillis
+		while (!getLocationTask.isComplete && timeRemaining > 0) {
+			delay(pauseMillis)
+			timeRemaining -= pauseMillis
+		}
+	}
+
+	private fun checkForErrorsError(getLocationTask: Task<android.location.Location>) {
+		if (!getLocationTask.isComplete) {
+			throw UserFriendlyException(R.string.error_could_not_determine_location, "Timed out after $timeoutMillis ms")
+		}
+		getLocationTask.exception?.let {
+			throw UserFriendlyException(R.string.error_could_not_determine_location, it)
+		}
+		if (getLocationTask.result == null) {
+			throw UserFriendlyException(R.string.error_could_not_determine_location, "Location API returned null")
+		}
+	}
+
+	companion object {
+	    private val pauseMillis = 50L
+		private val timeoutMillis = 5000L
+	}
 }
