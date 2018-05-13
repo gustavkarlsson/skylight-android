6b366c41db690a3ba0fd32e8bfc112bb052de15c
Gustav Karlsson <gustav.karlsson@gmail.com>
2018-05-13 18:20:20 +0200

    Created MainState

---
+++ app/src/main/kotlin/se/gustavkarlsson/skylight/android/gui/activities/main/MainState.kt
@@ -0,0 +1,19 @@
+package se.gustavkarlsson.skylight.android.gui.activities.main
+
+import se.gustavkarlsson.skylight.android.entities.Chance
+
+data class MainState(
+	val locationName: CharSequence,
+	val isRefreshing: Boolean,
+	val errorMessage: Int?,
+	val connectivityMessage: CharSequence?,
+	val chanceLevel: CharSequence,
+	val timeSinceUpdate: CharSequence,
+	val isTimeSinceUpdateVisibile: Boolean,
+	val darkness: Factor,
+	val geomagLocation: Factor,
+	val kpIndex: Factor,
+	val visibility: Factor
+) {
+	data class Factor(val value: CharSequence, val chance: Chance)
+}
