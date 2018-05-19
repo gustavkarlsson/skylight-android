afcda7de365a086385b09cdece37c9038b4e3b70
Gustav Karlsson <gustav.karlsson@gmail.com>
2018-05-19 13:35:04 +0200

    Fixed LifecycleActions

--- app/src/main/kotlin/se/gustavkarlsson/skylight/android/extensions/LifecycleActions.kt
+++ app/src/main/kotlin/se/gustavkarlsson/skylight/android/extensions/LifecycleActions.kt
@@ -6,59 +6,106 @@ import android.arch.lifecycle.LifecycleObserver
 import android.arch.lifecycle.LifecycleOwner
 import android.arch.lifecycle.OnLifecycleEvent
 
-private class OnCreate(private val action: () -> Unit) : LifecycleObserver {
+private abstract class OnAction(
+	action: () -> Unit,
+	lifecycleToRemoveFrom: Lifecycle?
+) : LifecycleObserver {
+	protected var action =
+		if (lifecycleToRemoveFrom == null) {
+			action
+		} else {
+			{
+				action()
+				lifecycleToRemoveFrom.removeObserver(this)
+			}
+		}
+}
 
+private class OnCreate(
+	action: () -> Unit,
+	lifecycleToRemoveFrom: Lifecycle? = null
+) : OnAction(action, lifecycleToRemoveFrom), LifecycleObserver {
 	@OnLifecycleEvent(ON_CREATE)
 	fun invoke() = action()
 }
 
-private class OnStart(private val action: () -> Unit) : LifecycleObserver {
-
+private class OnStart(
+	action: () -> Unit,
+	lifecycleToRemoveFrom: Lifecycle? = null
+) : OnAction(action, lifecycleToRemoveFrom), LifecycleObserver {
 	@OnLifecycleEvent(ON_START)
 	fun invoke() = action()
 }
 
-private class OnResume(private val action: () -> Unit) : LifecycleObserver {
-
+private class OnResume(
+	action: () -> Unit,
+	lifecycleToRemoveFrom: Lifecycle? = null
+) : OnAction(action, lifecycleToRemoveFrom), LifecycleObserver {
 	@OnLifecycleEvent(ON_RESUME)
 	fun invoke() = action()
 }
 
-private class OnPause(private val action: () -> Unit) : LifecycleObserver {
-
+private class OnPause(
+	action: () -> Unit,
+	lifecycleToRemoveFrom: Lifecycle? = null
+) : OnAction(action, lifecycleToRemoveFrom), LifecycleObserver {
 	@OnLifecycleEvent(ON_PAUSE)
 	fun invoke() = action()
 }
 
-private class OnStop(private val action: () -> Unit) : LifecycleObserver {
-
+private class OnStop(
+	action: () -> Unit,
+	lifecycleToRemoveFrom: Lifecycle? = null
+) : OnAction(action, lifecycleToRemoveFrom), LifecycleObserver {
 	@OnLifecycleEvent(ON_STOP)
 	fun invoke() = action()
 }
 
-private class OnDestroy(private val action: () -> Unit) : LifecycleObserver {
-
+private class OnDestroy(
+	action: () -> Unit,
+	lifecycleToRemoveFrom: Lifecycle? = null
+) : OnAction(action, lifecycleToRemoveFrom), LifecycleObserver {
 	@OnLifecycleEvent(ON_DESTROY)
 	fun invoke() = action()
 }
 
-private class OnAny(private val action: () -> Unit) : LifecycleObserver {
-
+private class OnAny(
+	action: () -> Unit,
+	lifecycleToRemoveFrom: Lifecycle? = null
+) : OnAction(action, lifecycleToRemoveFrom), LifecycleObserver {
 	@OnLifecycleEvent(ON_ANY)
 	fun invoke() = action()
 }
 
-fun doOnLifecycle(owner: LifecycleOwner, event: Lifecycle.Event, block: () -> Unit) {
-	when (event) {
-		ON_CREATE -> owner.lifecycle.addObserver(OnCreate(block))
-		ON_START -> owner.lifecycle.addObserver(OnStart(block))
-		ON_RESUME -> owner.lifecycle.addObserver(OnResume(block))
-		ON_PAUSE -> owner.lifecycle.addObserver(OnPause(block))
-		ON_STOP -> owner.lifecycle.addObserver(OnStop(block))
-		ON_DESTROY -> owner.lifecycle.addObserver(OnDestroy(block))
-		ON_ANY -> owner.lifecycle.addObserver(OnAny(block))
+fun doOn(owner: LifecycleOwner, event: Lifecycle.Event, block: () -> Unit) {
+	val onAction = when (event) {
+		ON_CREATE -> OnCreate(block)
+		ON_START -> OnStart(block)
+		ON_RESUME -> OnResume(block)
+		ON_PAUSE -> OnPause(block)
+		ON_STOP -> OnStop(block)
+		ON_DESTROY -> OnDestroy(block)
+		ON_ANY -> OnAny(block)
 	}
+	owner.lifecycle.addObserver(onAction)
 }
 
-fun <T> T.doOnLifecycle(owner: LifecycleOwner, event: Lifecycle.Event, block: (T) -> Unit) =
-	se.gustavkarlsson.skylight.android.extensions.doOnLifecycle(owner, event) { block(this) }
+fun <T> T.doOn(owner: LifecycleOwner, event: Lifecycle.Event, block: (T) -> Unit) =
+	se.gustavkarlsson.skylight.android.extensions.doOn(owner, event) { block(this) }
+
+fun doOnNext(owner: LifecycleOwner, event: Lifecycle.Event, block: () -> Unit) {
+	val lifecycle = owner.lifecycle
+	val onAction = when (event) {
+		ON_CREATE -> OnCreate(block, lifecycle)
+		ON_START -> OnStart(block, lifecycle)
+		ON_RESUME -> OnResume(block, lifecycle)
+		ON_PAUSE -> OnPause(block, lifecycle)
+		ON_STOP -> OnStop(block, lifecycle)
+		ON_DESTROY -> OnDestroy(block, lifecycle)
+		ON_ANY -> OnAny(block, lifecycle)
+	}
+	lifecycle.addObserver(onAction)
+}
+
+fun <T> T.doOnNext(owner: LifecycleOwner, event: Lifecycle.Event, block: (T) -> Unit) =
+	se.gustavkarlsson.skylight.android.extensions.doOnNext(owner, event) { block(this) }
