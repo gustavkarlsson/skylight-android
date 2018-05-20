4f1fd14c8895e904fbcadc0e7a01529fdf8d6168
Gustav Karlsson <gustav.karlsson@gmail.com>
2018-05-20 18:06:51 +0200

    Added support for adding multiple transformers at a time

--- app/src/main/kotlin/se/gustavkarlsson/skylight/android/flux/Store.kt
+++ app/src/main/kotlin/se/gustavkarlsson/skylight/android/flux/Store.kt
@@ -79,61 +79,61 @@ internal constructor(
 	private var observeScheduler: Scheduler? = null
 	private val startActions: MutableList<Action> = mutableListOf()
 
-	fun transformActions(transformer: (Observable<Action>) -> Observable<Result>) {
-		actionTransformers += transformer
+	fun transformActions(vararg transformers: (Observable<Action>) -> Observable<Result>) {
+		actionTransformers += transformers
 	}
 
 	inline fun <reified A : Action, reified R : Result> mapAction(noinline mapper: (A) -> R) {
-		transformActions { actions: Observable<Action> ->
+		transformActions({ actions: Observable<Action> ->
 			actions.ofType<A>().map(mapper)
-		}
+		})
 	}
 
 	inline fun <reified A : Action, reified R : Result> flatMapAction(
 		noinline mapper: (A) -> Observable<R>
 	) {
-		transformActions { actions: Observable<Action> ->
+		transformActions({ actions: Observable<Action> ->
 			actions.ofType<A>().flatMap(mapper)
-		}
+		})
 	}
 
 	inline fun <reified A : Action, reified R : Result> switchMapAction(
 		noinline mapper: (A) -> Observable<R>
 	) {
-		transformActions { actions: Observable<Action> ->
+		transformActions({ actions: Observable<Action> ->
 			actions.ofType<A>().switchMap(mapper)
-		}
+		})
 	}
 
-	fun transformActionsWithState(transformer: (Observable<State>, Observable<Action>) -> Observable<Result>) {
-		actionWithStateTransformers += transformer
+	fun transformActionsWithState(vararg transformers: (Observable<State>, Observable<Action>) -> Observable<Result>) {
+		actionWithStateTransformers += transformers
 	}
 
 	inline fun <reified A : Action, reified R : Result> mapActionWithState(noinline mapper: (State, A) -> R) {
 		val function = BiFunction<A, State, R> { action, state ->
 			mapper(state, action)
 		}
-		transformActionsWithState { states: Observable<State>, actions: Observable<Action> ->
+		transformActionsWithState({ states: Observable<State>, actions: Observable<Action> ->
 			actions.ofType<A>().withLatestFrom(states, function)
-		}
+		})
 	}
 
 	inline fun <reified A : Action, reified R : Result> flatMapActionWithState(noinline mapper: (State, A) -> Observable<R>) {
 		val function = BiFunction<A, State, Observable<R>> { action, state ->
 			mapper(state, action)
 		}
-		transformActionsWithState { states: Observable<State>, actions: Observable<Action> ->
+		transformActionsWithState({ states: Observable<State>, actions: Observable<Action> ->
 			actions.ofType<A>().withLatestFrom(states, function).flatMap { it }
-		}
+		})
 	}
 
 	inline fun <reified A : Action, reified R : Result> switchMapActionWithState(noinline mapper: (State, A) -> Observable<R>) {
 		val function = BiFunction<A, State, Observable<R>> { action, state ->
 			mapper(state, action)
 		}
-		transformActionsWithState { states: Observable<State>, actions: Observable<Action> ->
+		transformActionsWithState({ states: Observable<State>, actions: Observable<Action> ->
 			actions.ofType<A>().withLatestFrom(states, function).switchMap { it }
-		}
+		})
 	}
 
 	fun setObserveScheduler(scheduler: Scheduler?) {
