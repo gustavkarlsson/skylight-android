2af5e12243d14abd244c9a090a3d8f2b2780f5e3
Gustav Karlsson <gustav.karlsson@gmail.com>
2018-05-27 17:59:03 +0200

    Implemented new main layout with progress bars

--- app/src/main/kotlin/se/gustavkarlsson/skylight/android/gui/screens/main/MainFragment.kt
+++ app/src/main/kotlin/se/gustavkarlsson/skylight/android/gui/screens/main/MainFragment.kt
@@ -11,14 +11,11 @@ import android.support.v4.app.Fragment
 import android.support.v7.app.AlertDialog
 import android.view.*
 import com.jakewharton.rxbinding2.support.v4.widget.refreshes
-import com.jakewharton.rxbinding2.view.clicks
 import com.jakewharton.rxbinding2.view.visibility
 import com.jakewharton.rxbinding2.widget.text
 import com.tbruyelle.rxpermissions2.RxPermissions
 import com.uber.autodispose.android.lifecycle.scope
 import com.uber.autodispose.kotlin.autoDisposable
-import io.reactivex.Observable
-import io.reactivex.functions.Consumer
 import kotlinx.android.synthetic.main.fragment_main.*
 import org.jetbrains.anko.alert
 import org.jetbrains.anko.okButton
@@ -26,11 +23,8 @@ import org.jetbrains.anko.toast
 import se.gustavkarlsson.skylight.android.BuildConfig
 import se.gustavkarlsson.skylight.android.R
 import se.gustavkarlsson.skylight.android.appComponent
-import se.gustavkarlsson.skylight.android.entities.Chance
-import se.gustavkarlsson.skylight.android.extensions.appCompatActivity
 import se.gustavkarlsson.skylight.android.extensions.findNavController
 import se.gustavkarlsson.skylight.android.extensions.indefiniteErrorSnackbar
-import se.gustavkarlsson.skylight.android.gui.views.AuroraFactorView
 import se.gustavkarlsson.skylight.android.services.Analytics
 import timber.log.Timber
 
@@ -76,35 +70,32 @@ class MainFragment : Fragment(), LifecycleObserver {
 
 	@OnLifecycleEvent(Lifecycle.Event.ON_START)
 	private fun bindData() {
-		val disposeEvent = Lifecycle.Event.ON_STOP
-
+		val scope = scope(Lifecycle.Event.ON_STOP)
 		swipeRefreshLayout.refreshes()
 			.doOnNext {
 				Timber.i("Triggering refresh")
 				Analytics.logManualRefresh()
 			}
-			.autoDisposable(scope(disposeEvent))
+			.autoDisposable(scope)
 			.subscribe(viewModel.swipedToRefresh)
 
 		viewModel.locationName
 			.doOnNext { Timber.d("Updating locationName view: %s", it) }
-			.autoDisposable(scope(disposeEvent))
-			.subscribe {
-				appCompatActivity?.supportActionBar?.title = it
-			}
+			.autoDisposable(scope)
+			.subscribe(locationName.text())
 
 		viewModel.isRefreshing
 			.doOnNext { Timber.i("Refreshing: $it") }
-			.autoDisposable(scope(disposeEvent))
+			.autoDisposable(scope)
 			.subscribe(swipeRefreshLayout::setRefreshing)
 
 		viewModel.errorMessages
 			.doOnNext { Timber.d("Showing error message") }
-			.autoDisposable(scope(disposeEvent))
+			.autoDisposable(scope)
 			.subscribe { activity?.toast(it) }
 
 		viewModel.connectivityMessages
-			.autoDisposable(scope(disposeEvent))
+			.autoDisposable(scope)
 			.subscribe {
 				snackbar?.run {
 					Timber.d("Hiding connectivity message")
@@ -120,22 +111,22 @@ class MainFragment : Fragment(), LifecycleObserver {
 
 		viewModel.chanceLevel
 			.doOnNext { Timber.d("Updating chanceLevel view: %s", it) }
-			.autoDisposable(scope(disposeEvent))
+			.autoDisposable(scope)
 			.subscribe(chance.text())
 
 		viewModel.timeSinceUpdate
 			.doOnNext { Timber.d("Updating timeSinceUpdate view: %s", it) }
-			.autoDisposable(scope(disposeEvent))
+			.autoDisposable(scope)
 			.subscribe(timeSinceUpdate.text())
 
 		viewModel.timeSinceUpdateVisibility
 			.doOnNext { Timber.d("Updating timeSinceUpdate weather: %s", it) }
-			.autoDisposable(scope(disposeEvent))
+			.autoDisposable(scope)
 			.subscribe(timeSinceUpdate.visibility())
 
 		viewModel.showDialog
 			.doOnNext { Timber.d("Showing dialog: %s", it) }
-			.autoDisposable(scope(disposeEvent))
+			.autoDisposable(scope)
 			.subscribe {
 				dialog?.dismiss()
 				dialog = activity?.alert {
@@ -149,63 +140,44 @@ class MainFragment : Fragment(), LifecycleObserver {
 
 		viewModel.hideDialog
 			.doOnNext { Timber.d("Hiding dialog") }
-			.autoDisposable(scope(disposeEvent))
+			.autoDisposable(scope)
 			.subscribe { dialog?.dismiss() }
 
 		viewModel.ensureLocationPermission
 			.doOnNext { Timber.d("Location permission is unknown") }
-			.autoDisposable(scope(disposeEvent))
+			.autoDisposable(scope)
 			.subscribe {
 				ensureLocationPermission()
 			}
 
-		bindFactor(
-			viewModel.darknessValue, viewModel.darknessChance, darkness,
+		FactorPresenter(
+			viewModel.darknessValue, viewModel.darknessChance,
+			darknessValue, darknessBar, darknessCard,
 			viewModel.darknessFactorClicked,
-			disposeEvent,
+			scope,
 			"darkness"
-		)
-		bindFactor(
-			viewModel.geomagLocationValue, viewModel.geomagLocationChance, geomagLocation,
+		).present()
+		FactorPresenter(
+			viewModel.geomagLocationValue, viewModel.geomagLocationChance,
+			geomagLocationValue, geomagLocationBar, geomagLocationCard,
 			viewModel.geomagLocationFactorClicked,
-			disposeEvent,
+			scope,
 			"geomagLocation"
-		)
-		bindFactor(
-			viewModel.kpIndexValue, viewModel.kpIndexChance, kpIndex,
+		).present()
+		FactorPresenter(
+			viewModel.kpIndexValue, viewModel.kpIndexChance,
+			kpIndexValue, kpIndexBar, kpIndexCard,
 			viewModel.kpIndexFactorClicked,
-			disposeEvent,
+			scope,
 			"kpIndex"
-		)
-		bindFactor(
-			viewModel.weatherValue, viewModel.weatherChance, weather,
+		).present()
+		FactorPresenter(
+			viewModel.weatherValue, viewModel.weatherChance,
+			weatherValue, weatherBar, weatherCard,
 			viewModel.weatherFactorClicked,
-			disposeEvent,
+			scope,
 			"weather"
-		)
-	}
-
-	private fun bindFactor(
-		values: Observable<CharSequence>,
-		chances: Observable<Chance>,
-		view: AuroraFactorView,
-		clickConsumer: Consumer<Unit>,
-		disposeEvent: Lifecycle.Event,
-		factorDebugName: String
-	) {
-		values
-			.doOnNext { Timber.d("Updating %s value view: %s", factorDebugName, it) }
-			.autoDisposable(scope(disposeEvent))
-			.subscribe { view.value = it }
-
-		chances
-			.doOnNext { Timber.d("Updating %s chance view: %s", factorDebugName, it) }
-			.autoDisposable(scope(disposeEvent))
-			.subscribe { view.chance = it }
-
-		view.clicks()
-			.autoDisposable(scope(disposeEvent))
-			.subscribe(clickConsumer)
+		).present()
 	}
 
 	private fun ensureLocationPermission() {
