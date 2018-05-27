8ea6d95d8ff2e0441229743f13977be10996d6c1
Gustav Karlsson <gustav.karlsson@gmail.com>
2018-05-27 19:19:00 +0200

    Added support annotation to UserFriendlyException

--- app/src/main/kotlin/se/gustavkarlsson/skylight/android/util/UserFriendlyException.kt
+++ app/src/main/kotlin/se/gustavkarlsson/skylight/android/util/UserFriendlyException.kt
@@ -1,21 +1,23 @@
 package se.gustavkarlsson.skylight.android.util
 
+import android.support.annotation.IdRes
+
 class UserFriendlyException : RuntimeException {
     val stringResourceId: Int
 
-    constructor(stringResourceId: Int, cause: Throwable) : super(cause) {
+    constructor(@IdRes stringResourceId: Int, cause: Throwable) : super(cause) {
         this.stringResourceId = stringResourceId
     }
 
-    constructor(stringResourceId: Int, message: String, cause: Throwable) : super(message, cause) {
+    constructor(@IdRes stringResourceId: Int, message: String, cause: Throwable) : super(message, cause) {
         this.stringResourceId = stringResourceId
     }
 
-    constructor(stringResourceId: Int, message: String) : super(message) {
+    constructor(@IdRes stringResourceId: Int, message: String) : super(message) {
         this.stringResourceId = stringResourceId
     }
 
-    constructor(stringResourceId: Int) {
+    constructor(@IdRes stringResourceId: Int) {
         this.stringResourceId = stringResourceId
     }
 }
