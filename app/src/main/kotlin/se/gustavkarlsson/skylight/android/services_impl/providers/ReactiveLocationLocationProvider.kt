70f1a7846d05f0a43f62391a329e5fe198ebdf71
Gustav Karlsson <gustav.karlsson@gmail.com>
2018-08-26 17:03:46 +0200

    Fixed location provider

--- app/src/main/kotlin/se/gustavkarlsson/skylight/android/services_impl/providers/ReactiveLocationLocationProvider.kt
+++ app/src/main/kotlin/se/gustavkarlsson/skylight/android/services_impl/providers/ReactiveLocationLocationProvider.kt
@@ -1,7 +1,6 @@
 package se.gustavkarlsson.skylight.android.services_impl.providers
 
 import android.annotation.SuppressLint
-import com.google.android.gms.location.LocationRequest
 import com.hadisatrio.optional.Optional
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
@@ -11,22 +10,19 @@ import se.gustavkarlsson.skylight.android.entities.Location
 import se.gustavkarlsson.skylight.android.extensions.seconds
 import se.gustavkarlsson.skylight.android.services.providers.LocationProvider
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 class ReactiveLocationLocationProvider(
 	private val reactiveLocationProvider: ReactiveLocationProvider,
 	private val timeout: Duration = 5.seconds
 ) : LocationProvider {
-	private val locationRequest = LocationRequest().apply {
-		priority = LocationRequest.PRIORITY_LOW_POWER
-		numUpdates = 1
-		maxWaitTime = timeout.toMillis()
-	}
 
 	@SuppressLint("MissingPermission")
 	override fun get(): Single<Optional<Location>> {
-		return reactiveLocationProvider.getUpdatedLocation(locationRequest)
+		return reactiveLocationProvider.lastKnownLocation
 			.subscribeOn(Schedulers.io())
-			.singleOrError()
+			.firstOrError()
+			.timeout(timeout.toMillis(), TimeUnit.MILLISECONDS)
 			.map { Optional.of(Location(it.latitude, it.longitude)) }
 			.doOnError { Timber.w(it, "Failed to get location") }
 			.onErrorReturnItem(Optional.absent())
