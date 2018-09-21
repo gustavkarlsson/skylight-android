bfed06758f96384dfdb395bdcf2b3c23ad56d640
Gustav Karlsson <gustav.karlsson@gmail.com>
2018-09-21 19:56:21 +0200

    Fixed forcing location by switching to GPS provider and LocationManager

--- app/src/main/kotlin/se/gustavkarlsson/skylight/android/modules/LocationModule.kt
+++ app/src/main/kotlin/se/gustavkarlsson/skylight/android/modules/LocationModule.kt
@@ -1,6 +1,8 @@
 package se.gustavkarlsson.skylight.android.modules
 
 import android.Manifest
+import android.content.Context
+import android.location.LocationManager
 import io.reactivex.Flowable
 import org.koin.dsl.module.module
 import pl.charmas.android.reactivelocation2.ReactiveLocationProvider
@@ -9,17 +11,21 @@ import se.gustavkarlsson.skylight.android.extensions.minutes
 import se.gustavkarlsson.skylight.android.extensions.seconds
 import se.gustavkarlsson.skylight.android.services.Streamable
 import se.gustavkarlsson.skylight.android.services.providers.LocationProvider
-import se.gustavkarlsson.skylight.android.services_impl.providers.ReactiveLocationLocationProvider
+import se.gustavkarlsson.skylight.android.services_impl.providers.LocationManagerLocationProvider
 import se.gustavkarlsson.skylight.android.services_impl.streamables.ReactiveLocationProviderStreamable
 
 val locationModule = module {
+
+	single {
+		get<Context>().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+	}
 
 	single {
 		ReactiveLocationProvider(get())
 	}
 
 	single<LocationProvider> {
-		ReactiveLocationLocationProvider(get(), 5.seconds)
+		LocationManagerLocationProvider(get(), 20.seconds)
 	}
 
 	single<Streamable<Location>>("location") {
@@ -34,7 +40,7 @@ val locationModule = module {
 	}
 
 	single("locationPermission") {
-		Manifest.permission.ACCESS_COARSE_LOCATION
+		Manifest.permission.ACCESS_FINE_LOCATION
 	}
 
 }
