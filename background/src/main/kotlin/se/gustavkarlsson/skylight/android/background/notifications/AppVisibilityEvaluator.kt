fab448e9ca4f6ccc4ae719dcdf8373e8fdb4e07e
Gustav Karlsson <gustav.karlsson@gmail.com>
2018-09-30 18:18:49 +0200

    Fixed deprecated KeyguardManager method

--- background/src/main/kotlin/se/gustavkarlsson/skylight/android/background/notifications/AppVisibilityEvaluator.kt
+++ background/src/main/kotlin/se/gustavkarlsson/skylight/android/background/notifications/AppVisibilityEvaluator.kt
@@ -5,22 +5,23 @@ import android.app.ActivityManager.RunningAppProcessInfo
 import android.app.KeyguardManager
 
 internal class AppVisibilityEvaluator(
-        private val keyguardManager: KeyguardManager
+	private val keyguardManager: KeyguardManager
 ) {
 
-    fun isVisible(): Boolean {
-        val processInfo = getProcessInfo()
-        if (!isInForeground(processInfo)) return false
+	fun isVisible(): Boolean {
+		val processInfo = getProcessInfo()
+		if (!isInForeground(processInfo)) return false
 
-        // app is in foreground, but screen can be locked
-        return !keyguardManager.inKeyguardRestrictedInputMode()
-    }
+		// app is in foreground, but screen can be locked
+		return !keyguardManager.isKeyguardLocked
+	}
 
-    private fun getProcessInfo(): RunningAppProcessInfo {
-        val processInfo = RunningAppProcessInfo()
-        ActivityManager.getMyMemoryState(processInfo)
-        return processInfo
-    }
+	private fun getProcessInfo(): RunningAppProcessInfo {
+		val processInfo = RunningAppProcessInfo()
+		ActivityManager.getMyMemoryState(processInfo)
+		return processInfo
+	}
 
-    private fun isInForeground(processInfo: RunningAppProcessInfo) = processInfo.importance == RunningAppProcessInfo.IMPORTANCE_FOREGROUND
+	private fun isInForeground(processInfo: RunningAppProcessInfo) =
+		processInfo.importance == RunningAppProcessInfo.IMPORTANCE_FOREGROUND
 }
