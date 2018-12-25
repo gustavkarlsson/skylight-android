df5b4cb473562a1160d367fbd0da55d5f74019ef
Gustav Karlsson <gustav.karlsson@gmail.com>
2018-12-25 13:39:04 +0100

    Fixed lifecycle management of snackbars

--- app/src/main/kotlin/se/gustavkarlsson/skylight/android/gui/screens/googleplayservices/GooglePlayServicesFragment.kt
+++ app/src/main/kotlin/se/gustavkarlsson/skylight/android/gui/screens/googleplayservices/GooglePlayServicesFragment.kt
@@ -1,5 +1,6 @@
 package se.gustavkarlsson.skylight.android.gui.screens.googleplayservices
 
+import androidx.lifecycle.Lifecycle.Event.ON_DESTROY
 import com.google.android.material.snackbar.Snackbar
 import com.jakewharton.rxbinding2.view.clicks
 import com.uber.autodispose.LifecycleScopeProvider
@@ -7,13 +8,12 @@ import com.uber.autodispose.kotlin.autoDisposable
 import kotlinx.android.synthetic.main.fragment_google_play_services.installButton
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import se.gustavkarlsson.skylight.android.R
+import se.gustavkarlsson.skylight.android.extensions.doOnNext
 import se.gustavkarlsson.skylight.android.extensions.showErrorSnackbar
 import se.gustavkarlsson.skylight.android.gui.BaseFragment
 import timber.log.Timber
 
 class GooglePlayServicesFragment : BaseFragment(R.layout.fragment_google_play_services) {
-
-	private var errorSnackbar: Snackbar? = null
 
 	private val viewModel: GooglePlayServicesViewModel by viewModel()
 
@@ -27,22 +27,15 @@ class GooglePlayServicesFragment : BaseFragment(R.layout.fragment_google_play_se
 				viewModel.signalGooglePlayServicesInstalled()
 			}, {
 				Timber.e(it, "Failed to install Google Play Services")
-				errorSnackbar?.run {
-					Timber.d("Hiding previous error message")
-					dismiss()
-				}
 				view?.let { view ->
-					errorSnackbar = showErrorSnackbar(
+					showErrorSnackbar(
 						view,
 						R.string.google_play_services_install_failed,
 						Snackbar.LENGTH_LONG
-					)
+					).doOnNext(this, ON_DESTROY) { snackbar ->
+						snackbar.dismiss()
+					}
 				}
 			})
-	}
-
-	override fun onDestroy() {
-		errorSnackbar?.dismiss()
-		super.onDestroy()
 	}
 }
