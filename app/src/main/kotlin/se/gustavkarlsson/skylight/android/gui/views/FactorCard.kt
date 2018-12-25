6723a39770271c69fd34b33963adee952db8a01e
Gustav Karlsson <gustav.karlsson@gmail.com>
2018-12-25 14:49:29 +0100

    Upgraded to MaterialCardView

--- app/src/main/kotlin/se/gustavkarlsson/skylight/android/gui/views/FactorCard.kt
+++ app/src/main/kotlin/se/gustavkarlsson/skylight/android/gui/views/FactorCard.kt
@@ -1,11 +1,14 @@
 package se.gustavkarlsson.skylight.android.gui.views
 
 import android.content.Context
+import android.os.Build
 import android.util.AttributeSet
 import android.widget.FrameLayout
 import android.widget.ProgressBar
 import android.widget.TextView
-import androidx.cardview.widget.CardView
+import androidx.annotation.RequiresApi
+import com.google.android.material.card.MaterialCardView
+import kotlinx.android.synthetic.main.layout_factor_card.view.card
 import kotlinx.android.synthetic.main.layout_factor_card.view.progressBar
 import kotlinx.android.synthetic.main.layout_factor_card.view.title
 import kotlinx.android.synthetic.main.layout_factor_card.view.value
@@ -29,13 +32,24 @@ class FactorCard : FrameLayout {
 	}
 
 	private fun init(context: Context, attrs: AttributeSet?) {
-		val view = inflate(getContext(), R.layout.layout_factor_card, null) as CardView
+		val view = inflate(getContext(), R.layout.layout_factor_card, null) as MaterialCardView
 		addView(view)
 		context.theme.obtainStyledAttributes(attrs, R.styleable.FactorCard, 0, 0).apply {
 			titleView.text = getString(R.styleable.FactorCard_title)
 			recycle()
 		}
 	}
+
+	override fun hasOnClickListeners() = card.hasOnClickListeners()
+
+	override fun setOnClickListener(listener: OnClickListener?) = card.setOnClickListener(listener)
+
+	override fun setOnLongClickListener(listener: OnLongClickListener?) =
+		card.setOnLongClickListener(listener)
+
+	@RequiresApi(Build.VERSION_CODES.M)
+	override fun setOnContextClickListener(listener: OnContextClickListener?) =
+		card.setOnContextClickListener(listener)
 
 	private val titleView: TextView
 		get() = title
