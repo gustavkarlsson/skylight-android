7abed09efb0d898978255bd381b2c3567feca8b8
Gustav Karlsson <gustav.karlsson@gmail.com>
2019-05-05 19:16:46 +0200

    Cleaned up main viewmodel

--- app/src/main/kotlin/se/gustavkarlsson/skylight/android/util/ChanceToColorConverter.kt
+++ app/src/main/kotlin/se/gustavkarlsson/skylight/android/util/ChanceToColorConverter.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.graphics.Color
 import androidx.core.graphics.ColorUtils
 import se.gustavkarlsson.skylight.android.R
-import se.gustavkarlsson.skylight.android.entities.Chance
 import se.gustavkarlsson.skylight.android.extensions.toArgb
 
 class ChanceToColorConverter(context: Context) {
@@ -12,22 +11,22 @@ class ChanceToColorConverter(context: Context) {
 	private val middleColor = R.color.chance_middle.toArgb(context)
 	private val highestColor = R.color.chance_highest.toArgb(context)
 
-	fun convert(chance: Chance): Int {
-        if (!chance.isKnown) {
-            return Color.TRANSPARENT
-        }
+	fun convert(chance: Double?): Int {
+		if (chance == null) {
+			return Color.TRANSPARENT
+		}
 
-		val value = chance.value!!.toFloat()
-		return if (value < MIDDLE_COLOR_CHANCE) {
-			val blendAmount = value / MIDDLE_COLOR_CHANCE
+		return if (chance < MIDDLE_COLOR_CHANCE) {
+			val blendAmount = (chance / MIDDLE_COLOR_CHANCE).toFloat()
 			ColorUtils.blendARGB(lowestColor, middleColor, blendAmount)
 		} else {
-			val blendAmount = (value - MIDDLE_COLOR_CHANCE) / MIDDLE_COLOR_CHANCE
+			val blendAmount = ((chance - MIDDLE_COLOR_CHANCE) / MIDDLE_COLOR_CHANCE).toFloat()
 			ColorUtils.blendARGB(middleColor, highestColor, blendAmount)
 		}
-    }
+	}
 
-	private companion object {
-		const val MIDDLE_COLOR_CHANCE = 0.5F
+	companion object {
+		private const val MIDDLE_COLOR_CHANCE = 0.5
+		const val UNKNOWN_COLOR = Color.TRANSPARENT
 	}
 }
