bee4b4b4ad8541bf3a82bf59b2517633efdb9449
Gustav Karlsson <gustav.karlsson@gmail.com>
2019-09-15 23:25:13 +0200

    Fixed compilation error

--- core/src/main/java/se/gustavkarlsson/skylight/android/extensions/Koin.kt
+++ core/src/main/java/se/gustavkarlsson/skylight/android/extensions/Koin.kt
@@ -5,6 +5,6 @@ import org.koin.android.ext.android.inject
 import org.koin.core.parameter.parametersOf
 import org.koin.core.scope.Scope
 
-inline fun <reified T : Any> ComponentCallbacks.addToKoin(value: T, name: String? = null, scope: Scope? = null) {
+inline fun <reified T : Any> ComponentCallbacks.addToKoin(value: T, name: String = "", scope: Scope? = null) {
 	inject<T>(name = name, scope = scope) { parametersOf(value) }.value
 }
