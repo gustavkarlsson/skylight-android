f3a1a8749ac456a89147cbe1d09b3d156ee30701
Gustav Karlsson <gustav.karlsson@gmail.com>
2020-02-17 19:32:54 +0100

    Removed a lot of instrumentation test stuff

--- app/src/androidTest/java/se/gustavkarlsson/skylight/android/gui/screens/googleplayservices/GooglePlayServicesScreen.kt
+++
@@ -1,9 +0,0 @@
-package se.gustavkarlsson.skylight.android.gui.screens.googleplayservices
-
-import com.agoda.kakao.KButton
-import com.agoda.kakao.Screen
-import se.gustavkarlsson.skylight.android.R
-
-object GooglePlayServicesScreen : Screen<GooglePlayServicesScreen>() {
-	val installButton = KButton { withId(R.id.installButton) }
-}
