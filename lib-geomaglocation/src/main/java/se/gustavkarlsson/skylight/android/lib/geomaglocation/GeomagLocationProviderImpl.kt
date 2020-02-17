84db4ccf17cd11c3a8ff89419ef2d0a3307add85
Gustav Karlsson <gustav.karlsson@gmail.com>
2020-02-17 23:29:04 +0100

    Cleaned up GeomagLocationProviderImpl

--- lib-geomaglocation/src/main/java/se/gustavkarlsson/skylight/android/lib/geomaglocation/GeomagLocationProviderImpl.kt
+++ lib-geomaglocation/src/main/java/se/gustavkarlsson/skylight/android/lib/geomaglocation/GeomagLocationProviderImpl.kt
@@ -11,13 +11,13 @@ import se.gustavkarlsson.skylight.android.services.GeomagLocationProvider
 import se.gustavkarlsson.skylight.android.services.Time
 import timber.log.Timber
 import java.lang.Math.PI
-import java.lang.Math.atan2
-import java.lang.Math.cos
-import java.lang.Math.pow
-import java.lang.Math.sin
-import java.lang.Math.sqrt
 import java.lang.Math.toDegrees
 import java.lang.Math.toRadians
+import kotlin.math.atan2
+import kotlin.math.cos
+import kotlin.math.pow
+import kotlin.math.sin
+import kotlin.math.sqrt
 
 internal class GeomagLocationProviderImpl(
     private val time: Time
@@ -50,9 +50,7 @@ internal class GeomagLocationProviderImpl(
             onSuccess = {
                 val geomagneticLatitude = calculateGeomagneticLatitude(
                     it.latitude,
-                    it.longitude,
-                    MAGNETIC_NORTH_POLE_LATITUDE,
-                    MAGNETIC_NORTH_POLE_LONGITUDE
+                    it.longitude
                 )
                 Report.Success(GeomagLocation(geomagneticLatitude), time.now())
             },
@@ -65,25 +63,17 @@ internal class GeomagLocationProviderImpl(
         )
 
     // http://stackoverflow.com/a/7949249/940731
-    private fun calculateGeomagneticLatitude(
-        latitude1: Double,
-        longitude1: Double,
-        latitude2: Double,
-        longitude2: Double
-    ): Double {
-        var Dlong = longitude2
-        var Dlat = latitude2
-        val R = 1.0
-
+    private fun calculateGeomagneticLatitude(latitude1: Double, longitude1: Double): Double {
+        val r = 1.0
         // convert first to radians
-        Dlong = toRadians(Dlong)
-        Dlat = toRadians(Dlat)
+        val dLong = toRadians(MAGNETIC_NORTH_POLE_LONGITUDE)
+        val dLat = toRadians(MAGNETIC_NORTH_POLE_LATITUDE)
 
-        val glat = toRadians(latitude1)
-        val glon = toRadians(longitude1)
-        val galt = glat * 0.0 + R
+        val gLat = toRadians(latitude1)
+        val gLon = toRadians(longitude1)
+        val gAlt = gLat * 0.0 + r
 
-        val coord = doubleArrayOf(glat, glon, galt)
+        val coord = doubleArrayOf(gLat, gLon, gAlt)
 
         // convert to rectangular coordinates
         // X-axis: defined by the vector going from Earth's center towards the intersection of the equator and Greenwitch's meridian.
@@ -98,23 +88,23 @@ internal class GeomagLocationProviderImpl(
         // from the Greenwich meridian to the meridian containing the magnetic
         // dipole pole.
         val geolong2maglong = Array(3) { DoubleArray(3) }
-        geolong2maglong[0][0] = cos(Dlong)
-        geolong2maglong[0][1] = sin(Dlong)
-        geolong2maglong[1][0] = -sin(Dlong)
-        geolong2maglong[1][1] = cos(Dlong)
+        geolong2maglong[0][0] = cos(dLong)
+        geolong2maglong[0][1] = sin(dLong)
+        geolong2maglong[1][0] = -sin(dLong)
+        geolong2maglong[1][1] = cos(dLong)
         geolong2maglong[2][2] = 1.0
         var out = dotProduct(geolong2maglong, xyz)
 
         // Second rotation : in the plane of the current meridian from geographic pole to magnetic dipole pole.
         val tomaglat = Array(3) { DoubleArray(3) }
-        tomaglat[0][0] = cos(PI / 2 - Dlat)
-        tomaglat[0][2] = -sin(PI / 2 - Dlat)
-        tomaglat[2][0] = sin(PI / 2 - Dlat)
-        tomaglat[2][2] = cos(PI / 2 - Dlat)
+        tomaglat[0][0] = cos(PI / 2 - dLat)
+        tomaglat[0][2] = -sin(PI / 2 - dLat)
+        tomaglat[2][0] = sin(PI / 2 - dLat)
+        tomaglat[2][2] = cos(PI / 2 - dLat)
         tomaglat[1][1] = 1.0
         out = dotProduct(tomaglat, out)
 
-        var mlat = atan2(out[2], sqrt(pow(out[0], 2.0) + pow(out[1], 2.0)))
+        var mlat = atan2(out[2], sqrt(out[0].pow(2.0) + out[1].pow(2.0)))
         mlat = toDegrees(mlat)
         return mlat
     }
@@ -122,8 +112,8 @@ internal class GeomagLocationProviderImpl(
     private fun dotProduct(a: Array<DoubleArray>, b: DoubleArray): DoubleArray {
         val n = b.size
         val c = DoubleArray(n)
-        for (i in 0..n - 1) {
-            for (j in 0..n - 1) {
+        for (i in 0 until n) {
+            for (j in 0 until n) {
                 c[i] += a[i][j] * b[j]
             }
         }
