843549128091e683aaa8594b9e1f97bae8e56260
Gustav Karlsson <gustav.karlsson@gmail.com>
2020-02-20 02:12:09 +0100

    Added dark and light mode

--- app/src/main/java/se/gustavkarlsson/skylight/android/Skylight.kt
+++ app/src/main/java/se/gustavkarlsson/skylight/android/Skylight.kt
@@ -1,5 +1,6 @@
 package se.gustavkarlsson.skylight.android
 
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.multidex.MultiDexApplication
 import com.crashlytics.android.Crashlytics
 import com.jakewharton.threetenabp.AndroidThreeTen
@@ -15,6 +16,7 @@ internal class Skylight : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         if (LeakCanary.isInAnalyzerProcess(this)) return
         LeakCanary.install(this)
         AndroidThreeTen.init(this)
