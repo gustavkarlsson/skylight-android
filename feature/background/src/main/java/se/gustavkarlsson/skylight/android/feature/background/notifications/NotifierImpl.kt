ce5845ecf1d6aaefd7afcbdb2d1837c54adf6164
Gustav Karlsson <gustav.karlsson@gmail.com>
2020-07-11 13:23:25 +0200

    Remove unused import

--- feature/background/src/main/java/se/gustavkarlsson/skylight/android/feature/background/notifications/NotifierImpl.kt
+++ feature/background/src/main/java/se/gustavkarlsson/skylight/android/feature/background/notifications/NotifierImpl.kt
@@ -5,7 +5,6 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import androidx.core.app.NotificationCompat
 import se.gustavkarlsson.skylight.android.entities.ChanceLevel
 import se.gustavkarlsson.skylight.android.feature.background.R
