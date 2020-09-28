606e804e02d32ae747a4131e6fbabe63a7074cc4
Gustav Karlsson <gustav.karlsson@gmail.com>
2020-09-28 20:09:05 +0200

    Fix compilation error

--- app/src/main/java/se/gustavkarlsson/skylight/android/Skylight.kt
+++ app/src/main/java/se/gustavkarlsson/skylight/android/Skylight.kt
@@ -19,7 +19,6 @@ internal class Skylight : Application() {
         initStrictMode()
         initDarkMode()
         initThreeThen()
-        initRxJavaErrorHandling()
         initDagger(scope)
     }
 }
