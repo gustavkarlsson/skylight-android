373a566eafc173a24491fe5d57b993f7b27fc4d5
Gustav Karlsson <gustav.karlsson@gmail.com>
2021-02-28 10:43:00 +0100

    Make compose work by updating buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,5 +8,7 @@ repositories {
 }
 
 dependencies {
+    // Must also update in Versions.kt
     implementation("com.android.tools.build:gradle:7.0.0-alpha08")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
 }
