11f16010983d6f1cf9d63b6795a5c068d21ed42b
Gustav Karlsson <gustav.karlsson@ioki.com>
2021-04-10 13:57:47 +0200

    Make LineFactory more efficient

--- feature/main/src/main/java/se/gustavkarlsson/skylight/android/feature/main/view/Aurora.kt
+++ feature/main/src/main/java/se/gustavkarlsson/skylight/android/feature/main/view/Aurora.kt
@@ -20,6 +20,10 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.core.content.getSystemService
 import androidx.core.graphics.ColorUtils
 import kotlin.math.abs
@@ -29,17 +33,14 @@ import kotlin.random.Random
 
 private const val FALLBACK_REFRESH_RATE = 60
 private const val MAX_HUE = 360f
-private const val minLineWidth = 10f
-private const val maxLineWidth = 50f
-private val validLineWidth = minLineWidth..maxLineWidth // FIXME base on dps
 
 @Composable
 fun Aurora() {
     BoxWithConstraints(Modifier.fillMaxSize()) {
         val frameTime = FrameTime
-        val factory = LineFactory(canvasWidth, canvasHeight)
+        val factory = LineFactory(canvasWidth.toFloat(), canvasHeight.toFloat(), LocalDensity.current)
         var lines by remember {
-            val lineCount = canvasWidth / minLineWidth.toInt()
+            val lineCount = canvasWidth / 10
             val lines = List(lineCount) { factory.createLine(randomizeAge = true) }
             mutableStateOf(lines)
         }
@@ -59,33 +60,79 @@ fun Aurora() {
     }
 }
 
-private data class LineFactory(
-    private val canvasWidth: Int,
-    private val canvasHeight: Int,
+private class LineFactory(
+    canvasWidth: Float,
+    canvasHeight: Float,
+    density: Density,
+    minLineWidthDp: Dp = 8.dp,
+    maxLineWidthDp: Dp = 12.dp,
+    @FloatRange(from = 0.0, to = 1.0) yRandomness: Float = 0.4f,
+    @FloatRange(from = 0.0, to = 1.0) minHeightRatio: Float = 0.4f,
     private val color: Color = Color(0xFF4CFFA6),
     @FloatRange(from = 0.0, to = 1.0) private val hueRandomness: Float = 0.15f,
-    @FloatRange(from = 0.0, to = 1.0) private val yRandomness: Float = 0.4f,
-    @FloatRange(from = 0.0, to = 1.0) private val minHeightRatio: Float = 0.4f,
-    @FloatRange(from = 0.0) private val minTtlSeconds: Float = 2f,
-    @FloatRange(from = 0.0) private val maxTtlSeconds: Float = 8f,
+    @FloatRange(from = 0.0) minTtlSeconds: Float = 2f,
+    @FloatRange(from = 0.0) maxTtlSeconds: Float = 8f,
 ) {
-    fun createLine(randomizeAge: Boolean): Line {
+    init {
+        require(minLineWidthDp >= 0.dp) {
+            "minLineWidthDp ($minLineWidthDp) must be non-negative"
+        }
+        require(minLineWidthDp <= maxLineWidthDp) {
+            "minLineWidthDp ($minLineWidthDp) must be <= maxLineWidthDp ($maxLineWidthDp)"
+        }
+        require(yRandomness in VALID_RATIO) {
+            "yRandomness ($yRandomness) must be in $VALID_RATIO"
+        }
+        require(minHeightRatio in VALID_RATIO) {
+            "minHeightRatio ($minHeightRatio) must be in $VALID_RATIO"
+        }
+        require(hueRandomness in VALID_RATIO) {
+            "hueRandomness ($hueRandomness) must be in $VALID_RATIO"
+        }
+        require(minTtlSeconds > 0f) {
+            "minTtlSeconds ($minTtlSeconds) must be positive"
+        }
         require(minTtlSeconds <= maxTtlSeconds) {
             "minTtlSeconds ($minTtlSeconds) must be <= maxTtlSeconds ($maxTtlSeconds)"
         }
-        val yRandomnessPx = (canvasHeight * yRandomness)
+    }
+
+    private val xRange = 0f..canvasWidth
+    private val yRange = let {
+        val center = canvasHeight / 2
+        val delta = yRandomness / 2
+        val min = center - delta
+        val max = center + delta
+        min..max
+    }
+    private val yRandomness = (canvasHeight * yRandomness)
+    private val widthRange = with(density) {
+        val minPx = minLineWidthDp.toPx()
+        val maxPx = maxLineWidthDp.toPx()
+        minPx..maxPx
+    }
+    private val heightRange = let {
         val minLineHeight = (canvasHeight * minHeightRatio)
-        val maxLineHeight = (canvasHeight - (yRandomnessPx / 2))
-        val ttlSeconds = (minTtlSeconds..maxTtlSeconds).random()
+        val maxLineHeight = (canvasHeight - (this.yRandomness / 2))
+        minLineHeight..maxLineHeight
+    }
+    private val ttlSecondsRange = minTtlSeconds..maxTtlSeconds
+
+    fun createLine(randomizeAge: Boolean): Line {
+        val ttlSeconds = ttlSecondsRange.random()
         return Line(
-            x = (0..canvasWidth).random().toFloat(),
-            y = (canvasHeight / 2) - (yRandomnessPx / 2) + (Random.nextFloat() * yRandomnessPx),
-            width = validLineWidth.random(),
-            height = (minLineHeight..maxLineHeight).random(),
+            x = xRange.random(),
+            y = yRange.random(),
+            width = widthRange.random(),
+            height = heightRange.random(),
             color = color.randomizeHue(hueRandomness),
             ttlSeconds = ttlSeconds,
             ageSeconds = if (randomizeAge) (0f..ttlSeconds).random() else 0f,
         )
+    }
+
+    private companion object {
+        private val VALID_RATIO = 0f..1f
     }
 }
 
