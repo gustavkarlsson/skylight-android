0f3d69bb508e61e1460558b42e383898cb6348c5
Gustav Karlsson <gustav.karlsson@ioki.com>
2021-04-12 09:54:18 +0200

    Clean up Aurora effect

--- feature/main/src/main/java/se/gustavkarlsson/skylight/android/feature/main/view/Aurora.kt
+++ feature/main/src/main/java/se/gustavkarlsson/skylight/android/feature/main/view/Aurora.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.withFrameMillis
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
@@ -27,18 +28,17 @@ import androidx.core.graphics.ColorUtils
 import kotlin.math.abs
 import kotlin.math.min
 import kotlin.random.Random
-import se.gustavkarlsson.skylight.android.lib.ui.compose.ColorRange
-import se.gustavkarlsson.skylight.android.lib.ui.compose.rangeTo
 
 @Composable
 fun Aurora(
     modifier: Modifier = Modifier,
-    lineDistance: Dp = 5.dp,
-    lineWidthDps: ClosedRange<Dp> = 40.dp..80.dp,
+    flareDistance: Dp = 5.dp,
+    flareWidthDps: ClosedRange<Dp> = 40.dp..80.dp,
     @FloatRange(from = 0.0, to = 1.0) altitudeVariation: Float = 0.3f,
     @FloatRange(from = 0.0, to = 1.0) angleVariation: Float = 0.1f,
     @FloatRange(from = 0.0, to = 1.0) heightVariation: Float = 0.6f,
-    colors: ColorRange = Color(0xFF4CFF86)..Color(0xFF4CBFA6),
+    color1: Color = Color(0xFF4CFF86),
+    color2: Color = Color(0xFF4CBFA6),
     ttlMillis: LongRange = 2000L..8000L,
 ) {
     BoxWithConstraints(modifier.fillMaxSize()) {
@@ -46,26 +46,24 @@ fun Aurora(
             canvasWidth = constraints.maxWidth.toFloat(),
             canvasHeight = constraints.maxHeight.toFloat(),
             density = LocalDensity.current,
-            distance = lineDistance,
-            widthDps = lineWidthDps,
+            distance = flareDistance,
+            widthDps = flareWidthDps,
             altitudeVariation = altitudeVariation,
             angleVariation = angleVariation,
             heightVariation = heightVariation,
-            colors = colors,
+            colors = color1 to color2,
             ttlMillis = ttlMillis,
         )
-        EveryFrame { frameMillis ->
+        EveryFrame { timeMillis ->
             Canvas(Modifier.fillMaxSize()) {
-                renderer.render(this, frameMillis)
+                renderer.render(this, timeMillis)
             }
         }
     }
 }
 
 @Composable
-private fun EveryFrame(
-    content: @Composable (frameMillis: Long) -> Unit,
-) {
+private fun EveryFrame(content: @Composable (timeMillis: Long) -> Unit) {
     var frameTime by remember { mutableStateOf(0L) }
     if (frameTime > 0) {
         content(frameTime)
@@ -86,7 +84,7 @@ private class Renderer(
     altitudeVariation: Float,
     angleVariation: Float,
     private val heightVariation: Float,
-    private val colors: ColorRange,
+    private val colors: Pair<Color, Color>,
     private val ttlMillis: LongRange,
 ) {
     init {
@@ -102,7 +100,9 @@ private class Renderer(
         val widthDp = canvasWidth.toDp()
         (widthDp / distance).toInt()
     }
+
     private val xRange: ClosedRange<Float> = 0f..canvasWidth
+
     private val yRange: ClosedRange<Float> = let {
         val center = canvasHeight / 2
         val delta = (canvasHeight * altitudeVariation) / 2
@@ -110,15 +110,17 @@ private class Renderer(
         val max = center + delta
         min..max
     }
+
     private val widths: ClosedRange<Float> = with(density) {
         widthDps.map { it.toPx() }
     }
+
     private val angles: ClosedRange<Float> = let {
         val offset = angleVariation * 90f
         -offset..offset
     }
 
-    private fun createLine(index: Int, timeMillis: Long): Line {
+    private fun createFlare(index: Int, timeMillis: Long): Flare {
         val indexRandom = Random(index)
         val ttlMillis = ttlMillis.random(indexRandom)
         val generation = timeMillis / ttlMillis
@@ -126,7 +128,7 @@ private class Renderer(
         val instanceRandom = Random(instanceSeed)
         val y = yRange.random(instanceRandom)
         val heights: ClosedRange<Float> = createHeights(y)
-        return Line(
+        return Flare(
             x = xRange.random(instanceRandom),
             y = y,
             width = widths.random(instanceRandom),
@@ -148,10 +150,55 @@ private class Renderer(
 
     fun render(scope: DrawScope, timeMillis: Long) {
         repeat(count) { index ->
-            val line = createLine(index, timeMillis)
-            scope.draw(line)
+            val flare = createFlare(index, timeMillis)
+            scope.draw(flare)
         }
     }
+
+    private fun DrawScope.draw(flare: Flare) = with(flare) {
+        val steps = arrayOf(
+            0f to color.copy(alpha = color.alpha * fadeInOut(age)),
+            1f to Color.Transparent,
+        )
+        val higherThanWide = height > width
+        val smallestDimension = if (higherThanWide) width else height
+        val (scaleY, scaleX) = if (higherThanWide) {
+            (height / width) to 1.0f
+        } else 1.0f to (width / height)
+        val downScaledStartY = y - ((height / 2) / scaleY)
+        val downScaledEndY = y + ((height / 2) / scaleY)
+        val downScaledWidth = width / scaleX
+        val brush = Brush.radialGradient(
+            *steps,
+            center = Offset(x, y),
+            radius = smallestDimension / 2,
+        )
+        rotate(angle, Offset(x, y)) {
+            scale(scaleX, scaleY, Offset(x, y)) {
+                drawLine(
+                    brush,
+                    start = Offset(x, downScaledStartY),
+                    end = Offset(x, downScaledEndY),
+                    strokeWidth = downScaledWidth,
+                    blendMode = BlendMode.Screen,
+                )
+            }
+        }
+    }
+
+    private fun fadeInOut(age: Float): Float {
+        return abs((age + 0.5f) % 1f - 0.5f) / 0.5f
+    }
+
+    private data class Flare(
+        val x: Float,
+        val y: Float,
+        val width: Float,
+        val height: Float,
+        val angle: Float,
+        val color: Color,
+        val age: Float,
+    )
 }
 
 private fun <T : Comparable<T>, R : Comparable<R>> ClosedRange<T>.map(block: (T) -> R): ClosedRange<R> {
@@ -160,32 +207,22 @@ private fun <T : Comparable<T>, R : Comparable<R>> ClosedRange<T>.map(block: (T)
     return start..endInclusive
 }
 
+private fun Float.requirePositive(name: String) {
+    require(this > 0f) {
+        "$name ($this) must be positive"
+    }
+}
+
+private fun Float.requireRatio(name: String) {
+    require(this in 0f..1f) {
+        "$name ($this) must be a valid ratio (0.0-1.0)"
+    }
+}
+
 private fun <T> ClosedRange<T>.requirePositiveAndAscending(name: String) where T : Comparable<T>, T : Number {
-    require(start.toDouble() > 0.0) {
-        "$name ($this) must be positive"
+    require(start.toDouble() > 0.0 && start <= endInclusive) {
+        "$name ($this) must be positive and ascending"
     }
-    require(start <= endInclusive) {
-        "$name ($this) must be ascending"
-    }
-}
-
-private fun <T : Number> T.requirePositive(name: String) {
-    require(this.toDouble() > 0.0) {
-        "$name ($this) must be positive"
-    }
-}
-
-private fun <T : Number> T.requireRatio(name: String) {
-    require(this.toDouble() in 0.0..1.0) {
-        "$name ($this) must be within 0..1"
-    }
-}
-
-private fun ColorRange.random(random: Random): Color {
-    val startArgb = start.toArgb()
-    val endArgb = endInclusive.toArgb()
-    val colorInt = ColorUtils.blendARGB(startArgb, endArgb, random.nextFloat())
-    return Color(colorInt)
 }
 
 private fun ClosedRange<Float>.random(random: Random): Float {
@@ -193,41 +230,9 @@ private fun ClosedRange<Float>.random(random: Random): Float {
     return start + (random.nextFloat() * delta)
 }
 
-private fun DrawScope.draw(line: Line) = with(line) {
-    val steps = arrayOf(
-        0f to color.copy(alpha = color.alpha * fadeInOut(age)),
-        1f to Color.Transparent,
-    )
-    val higherThanWide = height > width
-    val smallestDimension = if (higherThanWide) width else height
-    val (scaleY, scaleX) = if (higherThanWide) {
-        (height / width) to 1.0f
-    } else 1.0f to (width / height)
-    val downScaledStartY = y - ((height / 2) / scaleY)
-    val downScaledEndY = y + ((height / 2) / scaleY)
-    val downScaledWidth = width / scaleX
-    val brush = Brush.radialGradient(
-        *steps,
-        center = Offset(x, y),
-        radius = smallestDimension / 2,
-    )
-    rotate(angle, Offset(x, y)) {
-        scale(scaleX, scaleY, Offset(x, y)) {
-            drawLine(brush, start = Offset(x, downScaledStartY), end = Offset(x, downScaledEndY), downScaledWidth)
-        }
-    }
+private fun Pair<Color, Color>.random(random: Random): Color {
+    val startArgb = first.toArgb()
+    val endArgb = second.toArgb()
+    val colorInt = ColorUtils.blendARGB(startArgb, endArgb, random.nextFloat())
+    return Color(colorInt)
 }
-
-private fun fadeInOut(age: Float): Float {
-    return abs((age + 0.5f) % 1f - 0.5f) / 0.5f
-}
-
-private data class Line(
-    val x: Float,
-    val y: Float,
-    val width: Float,
-    val height: Float,
-    val angle: Float,
-    val color: Color,
-    val age: Float,
-)
