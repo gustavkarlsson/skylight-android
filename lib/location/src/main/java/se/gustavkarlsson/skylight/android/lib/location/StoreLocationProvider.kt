cb5f472c84d0a374d84a70530240190a2791d53b
Gustav Karlsson <gustav.karlsson@gmail.com>
2021-06-30 22:35:46 +0200

    Make location provider take permission into account

--- lib/location/src/main/java/se/gustavkarlsson/skylight/android/lib/location/StoreLocationProvider.kt
+++ lib/location/src/main/java/se/gustavkarlsson/skylight/android/lib/location/StoreLocationProvider.kt
@@ -7,14 +7,20 @@ import com.dropbox.android.external.store4.fresh
 import com.dropbox.android.external.store4.get
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 import se.gustavkarlsson.skylight.android.core.entities.Loadable
 import se.gustavkarlsson.skylight.android.core.logging.logError
 import se.gustavkarlsson.skylight.android.core.logging.logInfo
+import se.gustavkarlsson.skylight.android.lib.permissions.Access
+import se.gustavkarlsson.skylight.android.lib.permissions.Permission
+import se.gustavkarlsson.skylight.android.lib.permissions.PermissionChecker
 
 internal class StoreLocationProvider(
     private val store: Store<Unit, LocationResult>,
+    private val permissionChecker: PermissionChecker,
 ) : LocationProvider {
     override suspend fun get(fresh: Boolean): LocationResult {
         val result = if (fresh) {
@@ -27,19 +33,30 @@ internal class StoreLocationProvider(
     }
 
     override fun stream(): Flow<Loadable<LocationResult>> =
-        store.stream(StoreRequest.cached(Unit, refresh = false))
-            .map { response ->
-                when (response) {
-                    is StoreResponse.Loading -> Loadable.loading()
-                    is StoreResponse.Data -> Loadable.loaded(response.value)
-                    is StoreResponse.Error.Exception -> {
-                        logError(response.error) { "Failed to get location request" }
-                        Loadable.loaded(LocationResult.errorUnknown())
-                    }
-                    is StoreResponse.Error.Message, is StoreResponse.NoNewData ->
-                        error("Unsupported response type: $response")
-                }
+        permissionChecker.permissions
+            .map { permissions ->
+                permissions[Permission.Location] == Access.Granted
             }
             .distinctUntilChanged()
-            .onEach { logInfo { "Streamed location: $it" } }
+            .flatMapLatest { permissionGranted ->
+                if (permissionGranted) {
+                    doStream()
+                } else flowOf(Loadable.loading())
+            }
+
+    private fun doStream() = store.stream(StoreRequest.cached(Unit, refresh = false))
+        .map { response ->
+            when (response) {
+                is StoreResponse.Loading -> Loadable.loading()
+                is StoreResponse.Data -> Loadable.loaded(response.value)
+                is StoreResponse.Error.Exception -> {
+                    logError(response.error) { "Failed to get location request" }
+                    Loadable.loaded(LocationResult.errorUnknown())
+                }
+                is StoreResponse.Error.Message, is StoreResponse.NoNewData ->
+                    error("Unsupported response type: $response")
+            }
+        }
+        .distinctUntilChanged()
+        .onEach { logInfo { "Streamed location: $it" } }
 }
