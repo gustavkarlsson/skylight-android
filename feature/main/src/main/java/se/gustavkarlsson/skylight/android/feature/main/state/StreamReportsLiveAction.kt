0da456ca76391ed193f7e6f740c0e8338d3df77b
Gustav Karlsson <gustav.karlsson@gmail.com>
2021-06-30 22:07:50 +0200

    Don't stream updates when current location and permission missing

--- feature/main/src/main/java/se/gustavkarlsson/skylight/android/feature/main/state/StreamReportsLiveAction.kt
+++ feature/main/src/main/java/se/gustavkarlsson/skylight/android/feature/main/state/StreamReportsLiveAction.kt
@@ -16,6 +16,8 @@ import se.gustavkarlsson.skylight.android.lib.aurora.AuroraReportProvider
 import se.gustavkarlsson.skylight.android.lib.aurora.LoadableAuroraReport
 import se.gustavkarlsson.skylight.android.lib.location.LocationProvider
 import se.gustavkarlsson.skylight.android.lib.location.LocationResult
+import se.gustavkarlsson.skylight.android.lib.permissions.Access
+import se.gustavkarlsson.skylight.android.lib.permissions.Permission
 import se.gustavkarlsson.skylight.android.lib.places.Place
 import javax.inject.Inject
 
@@ -25,32 +27,40 @@ internal class StreamReportsLiveAction @Inject constructor(
     @StreamThrottle private val throttleDuration: Duration,
 ) : Action<State> {
     override suspend fun execute(stateFlow: AtomicStateFlow<State>) {
-        isStoreLive(stateFlow).collectLatest { live ->
+        stateFlow.isLive.collectLatest { live ->
             if (live) {
                 streamAndUpdateReports(stateFlow)
             }
         }
     }
 
-    private fun isStoreLive(state: AtomicStateFlow<State>): Flow<Boolean> =
-        state.storeSubscriberCount
+    private val AtomicStateFlow<State>.isLive: Flow<Boolean>
+        get() = storeSubscriberCount
             .map { it > 0 }
             .distinctUntilChanged()
 
-    private suspend fun streamAndUpdateReports(state: AtomicStateFlow<State>) {
-        selectedPlace(state).collectLatest { selectedPlace ->
-            auroraReportProvider.stream(locationUpdates(selectedPlace))
-                .throttle(throttleDuration.toMillis())
-                .collectLatest { report ->
-                    state.update(selectedPlace, report)
-                }
+    private suspend fun streamAndUpdateReports(stateFlow: AtomicStateFlow<State>) {
+        placeToStream(stateFlow).collectLatest { place ->
+            if (place != null) {
+                auroraReportProvider.stream(locationUpdates(place))
+                    .throttle(throttleDuration.toMillis())
+                    .collectLatest { report ->
+                        stateFlow.update(place, report)
+                    }
+            }
         }
     }
 
-    private fun selectedPlace(state: AtomicStateFlow<State>): Flow<Place> =
-        state
-            .map { it.selectedPlace }
-            .distinctUntilChangedBy { selected -> selected.id }
+    private fun placeToStream(stateFlow: AtomicStateFlow<State>): Flow<Place?> =
+        stateFlow
+            .map { state ->
+                val currentSelected = state.selectedPlace == Place.Current
+                val permissionGranted = state.permissions[Permission.Location] == Access.Granted
+                if (currentSelected && !permissionGranted) {
+                    null
+                } else state.selectedPlace
+            }
+            .distinctUntilChangedBy { selected -> selected?.id }
 
     private fun locationUpdates(selectedPlace: Place): Flow<Loadable<LocationResult>> {
         return when (selectedPlace) {
