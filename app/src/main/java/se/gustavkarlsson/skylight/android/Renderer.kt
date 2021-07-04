d826c39f5d2b387159de5551986b1dae634720e8
Gustav Karlsson <gustav.karlsson@gmail.com>
2021-07-04 15:28:58 +0200

    Fix navigation bug with temporary fix

--- app/src/main/java/se/gustavkarlsson/skylight/android/Renderer.kt
+++ app/src/main/java/se/gustavkarlsson/skylight/android/Renderer.kt
@@ -2,13 +2,18 @@ package se.gustavkarlsson.skylight.android
 
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.Crossfade
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import com.zachklipp.compose.backstack.Backstack
 import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import se.gustavkarlsson.skylight.android.lib.navigation.Backstack
 import se.gustavkarlsson.skylight.android.lib.navigation.Navigator
+import se.gustavkarlsson.skylight.android.lib.navigation.Screen
 import se.gustavkarlsson.skylight.android.lib.ui.compose.collectAsLifecycleAwareState
 import se.gustavkarlsson.skylight.android.transitions.CrossFadeZoom
 
@@ -23,12 +28,27 @@ internal class Renderer(
             }
             val change by navigator.backstackChanges.collectAsLifecycleAwareState()
             val backstack = change.new.takeIf { it.isNotEmpty() } ?: change.old
-            Backstack(
-                backstack = backstack,
-                transition = CrossFadeZoom,
-            ) { screen ->
-                screen.run { activity.Content(scope) }
-            }
+            val topScreen = backstack.lastOrNull()
+            // TODO Backstack crashes when navigating to GooglePlayServicesScreen
+            //  Key -35963410 was used multiple times
+            RenderSimple(topScreen, scope)
+        }
+    }
+
+    @Composable
+    private fun RenderSimple(screen: Screen?, scope: CoroutineScope) {
+        Crossfade(targetState = screen) { screenToRender ->
+            screenToRender?.run { activity.Content(scope) }
+        }
+    }
+
+    @Composable
+    private fun RenderBackstack(backstack: Backstack, scope: CoroutineScope) {
+        Backstack(
+            backstack = backstack,
+            transition = CrossFadeZoom,
+        ) { screen ->
+            screen.run { activity.Content(scope) }
         }
     }
 }
