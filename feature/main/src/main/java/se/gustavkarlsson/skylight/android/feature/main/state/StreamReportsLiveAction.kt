cab80311eccf81cbcdfa83e85df3bdf9ab1d5155
Gustav Karlsson <gustav.karlsson@gmail.com>
2021-07-11 10:55:04 +0200

    Rework StreamReportsLiveAction to use state for current location

--- feature/main/src/main/java/se/gustavkarlsson/skylight/android/feature/main/state/StreamReportsLiveAction.kt
+++ feature/main/src/main/java/se/gustavkarlsson/skylight/android/feature/main/state/StreamReportsLiveAction.kt
@@ -1,9 +1,11 @@
 package se.gustavkarlsson.skylight.android.feature.main.state
 
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.distinctUntilChangedBy
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.mapNotNull
@@ -15,64 +17,63 @@ import se.gustavkarlsson.skylight.android.core.utils.throttle
 import se.gustavkarlsson.skylight.android.feature.main.viewmodel.StreamThrottle
 import se.gustavkarlsson.skylight.android.lib.aurora.AuroraReportProvider
 import se.gustavkarlsson.skylight.android.lib.aurora.LoadableAuroraReport
-import se.gustavkarlsson.skylight.android.lib.location.LocationProvider
 import se.gustavkarlsson.skylight.android.lib.location.LocationResult
 import se.gustavkarlsson.skylight.android.lib.places.Place
 import javax.inject.Inject
 
+@OptIn(ExperimentalCoroutinesApi::class)
 internal class StreamReportsLiveAction @Inject constructor(
-    private val locationProvider: LocationProvider,
     private val auroraReportProvider: AuroraReportProvider,
     @StreamThrottle private val throttleDuration: Duration,
 ) : Action<State> {
     override suspend fun execute(stateFlow: AtomicStateFlow<State>) {
-        isStoreLive(stateFlow).collectLatest { live ->
-            if (live) {
-                streamAndUpdateReports(stateFlow)
+        stateFlow
+            .isLive()
+            .flatMapLatest { live ->
+                if (live) {
+                    stateFlow.updates()
+                } else emptyFlow()
             }
-        }
+            .throttle(throttleDuration.toMillis())
+            .collectLatest { updateData ->
+                stateFlow.tryUpdate(updateData)
+            }
     }
 
-    private fun isStoreLive(stateFlow: AtomicStateFlow<State>): Flow<Boolean> =
-        stateFlow.storeSubscriberCount
-            .map { it > 0 }
+    private fun AtomicStateFlow<State>.isLive(): Flow<Boolean> =
+        storeSubscriberCount
+            .map { count -> count > 0 }
             .distinctUntilChanged()
 
-    private suspend fun streamAndUpdateReports(stateFlow: AtomicStateFlow<State>) {
-        selectedPlace(stateFlow).collectLatest { selectedPlace ->
-            auroraReportProvider.stream(locationUpdates(selectedPlace))
-                .throttle(throttleDuration.toMillis())
-                .collectLatest { report ->
-                    stateFlow.update(selectedPlace, report)
-                }
-        }
-    }
+    private fun Flow<State>.updates(): Flow<UpdateData> =
+        selectedPlaces()
+            .flatMapLatest { selectedPlace ->
+                auroraReportProvider.stream(locations(selectedPlace))
+                    .map { report ->
+                        UpdateData(selectedPlace, report)
+                    }
+            }
 
-    private fun selectedPlace(stateFlow: AtomicStateFlow<State>): Flow<Place> =
-        stateFlow
-            .mapNotNull { state -> state.selectedPlace }
-            .distinctUntilChangedBy { selected -> selected.id }
+    private fun Flow<State>.selectedPlaces(): Flow<Place> =
+        mapNotNull { state -> state.selectedPlace }
+            .distinctUntilChanged()
 
-    private fun locationUpdates(selectedPlace: Place): Flow<Loadable<LocationResult>> {
-        return when (selectedPlace) {
-            Place.Current -> locationProvider.stream()
+    private fun Flow<State>.locations(selectedPlace: Place): Flow<Loadable<LocationResult>> =
+        when (selectedPlace) {
+            Place.Current -> this.map { state -> state.currentLocation }
             is Place.Saved -> flowOf(Loadable.loaded(LocationResult.success(selectedPlace.location)))
         }
-    }
 
-    private suspend fun AtomicStateFlow<State>.update(
-        reportPlace: Place,
-        report: LoadableAuroraReport
-    ) {
+    private suspend fun AtomicStateFlow<State>.tryUpdate(updateData: UpdateData) {
         update {
-            if (selectedPlace == reportPlace) {
+            if (updateData.place == selectedPlace) {
                 when (this) {
-                    is State.Loading -> copy(selectedAuroraReport = report)
-                    is State.Ready -> copy(selectedAuroraReport = report)
+                    is State.Loading -> copy(selectedAuroraReport = updateData.report)
+                    is State.Ready -> copy(selectedAuroraReport = updateData.report)
                 }
-            } else {
-                this
-            }
+            } else this
         }
     }
 }
+
+private data class UpdateData(val place: Place, val report: LoadableAuroraReport)
