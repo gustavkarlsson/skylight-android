0f1d3a19644b2f363db14b790abad0ced7906ee1
Gustav Karlsson <gustav.karlsson@gmail.com>
2021-07-16 20:29:48 +0200

    S can be null

--- lib/state/src/main/java/se/gustavkarlsson/skylight/android/lib/state/State.kt
+++ lib/state/src/main/java/se/gustavkarlsson/skylight/android/lib/state/State.kt
@@ -1,15 +1,15 @@
 package se.gustavkarlsson.skylight.android.lib.state
 
-class State private constructor(private val map: Map<Any, Any>) {
+class State private constructor(private val map: Map<Any, Any?>) {
     @Suppress("UNCHECKED_CAST")
-    operator fun <S : Any> get(key: StateKey<S>): S {
+    operator fun <S> get(key: StateKey<S>): S {
         return when (val value = map[key]) {
             null -> key.createState(this)
             else -> value as S
         }
     }
 
-    fun <S : Any> copy(key: StateKey<S>, value: S): State {
+    fun <S> copy(key: StateKey<S>, value: S): State {
         val mutableMap = map.toMutableMap()
         mutableMap[key] = value
         return State(mutableMap)
