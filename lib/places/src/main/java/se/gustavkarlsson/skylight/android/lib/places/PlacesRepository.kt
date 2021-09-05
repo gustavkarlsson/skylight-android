95063a0281819cd8589ac13bcc7ecdee26090bce
Gustav Karlsson <gustav.karlsson@gmail.com>
2021-09-05 22:30:49 +0200

    Add FIXME comment about limiting place count

--- lib/places/src/main/java/se/gustavkarlsson/skylight/android/lib/places/PlacesRepository.kt
+++ lib/places/src/main/java/se/gustavkarlsson/skylight/android/lib/places/PlacesRepository.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.flow.Flow
 import se.gustavkarlsson.skylight.android.lib.location.Location
 
 interface PlacesRepository {
+    // FIXME Don't use this function to limit number of places to keep
     suspend fun insert(name: String, location: Location): Place.Saved
     suspend fun updateLastChanged(placeId: PlaceId.Saved): Place.Saved
     fun stream(): Flow<NonEmptyList<Place>>
