e1180d4863bca412f3a693d935262ccf156b2f7d
Gustav Karlsson <gustav.karlsson@gmail.com>
2021-09-12 12:48:06 +0200

    Change FIXME comment to introduce swipe-to-delete

--- lib/places/src/main/java/se/gustavkarlsson/skylight/android/lib/places/PlacesRepository.kt
+++ lib/places/src/main/java/se/gustavkarlsson/skylight/android/lib/places/PlacesRepository.kt
@@ -5,8 +5,9 @@ import kotlinx.coroutines.flow.Flow
 import se.gustavkarlsson.skylight.android.lib.location.Location
 
 interface PlacesRepository {
-    // FIXME Don't use this function to limit number of places to keep
     suspend fun insert(name: String, location: Location): Place.Saved
+
+    // FIXME Swipe to delete?
     suspend fun delete(id: PlaceId.Saved): Boolean
     suspend fun updateLastChanged(placeId: PlaceId.Saved): Place.Saved
     fun stream(): Flow<NonEmptyList<Place>>
