b4f266628068c992a43b6b4d1306264c521b4b19
Gustav Karlsson <gustav.karlsson@gmail.com>
2022-01-22 14:55:04 +0100

    Clean up SettingsViewModel dagger injection

--- feature/settings/src/main/java/se/gustavkarlsson/skylight/android/feature/settings/SettingsComponent.kt
+++ feature/settings/src/main/java/se/gustavkarlsson/skylight/android/feature/settings/SettingsComponent.kt
@@ -1,14 +1,10 @@
 package se.gustavkarlsson.skylight.android.feature.settings
 
 import dagger.Component
-import dagger.Module
-import dagger.Provides
-import se.gustavkarlsson.skylight.android.lib.settings.SettingsRepository
 import se.gustavkarlsson.skylight.android.lib.settings.SettingsComponent as LibSettingsComponent
 
 @Component(
-    modules = [SettingsModule::class],
-    dependencies = [LibSettingsComponent::class]
+    dependencies = [LibSettingsComponent::class],
 )
 internal interface SettingsComponent {
     fun viewModel(): SettingsViewModel
@@ -19,14 +15,4 @@ internal interface SettingsComponent {
                 .settingsComponent(LibSettingsComponent.instance)
                 .build()
     }
-}
-
-@Module
-internal object SettingsModule {
-
-    // FIXME clean up
-    @Provides
-    fun viewModel(
-        settingsRepository: SettingsRepository,
-    ): SettingsViewModel = SettingsViewModel(settingsRepository)
 }
