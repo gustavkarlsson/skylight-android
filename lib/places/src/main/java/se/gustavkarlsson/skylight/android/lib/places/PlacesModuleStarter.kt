ebb5fb2c1241905078b76ea3d9ef6a9afd5a3ca8
Gustav Karlsson <gustav.karlsson@gmail.com>
2022-01-22 15:23:39 +0100

    Clean up LibPlacesModule dagger injections

--- lib/places/src/main/java/se/gustavkarlsson/skylight/android/lib/places/PlacesModuleStarter.kt
+++ lib/places/src/main/java/se/gustavkarlsson/skylight/android/lib/places/PlacesModuleStarter.kt
@@ -4,13 +4,15 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
+import se.gustavkarlsson.skylight.android.core.Global
 import se.gustavkarlsson.skylight.android.core.ModuleStarter
 import se.gustavkarlsson.skylight.android.lib.analytics.Analytics
+import javax.inject.Inject
 
-internal class PlacesModuleStarter(
+internal class PlacesModuleStarter @Inject constructor(
     private val placesRepository: PlacesRepository,
     private val analytics: Analytics,
-    private val scope: CoroutineScope,
+    @Global private val scope: CoroutineScope,
 ) : ModuleStarter {
     override suspend fun start() {
         scope.launch {
