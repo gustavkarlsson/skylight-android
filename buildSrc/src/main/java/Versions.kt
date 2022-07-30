a4fa6e541fae0cea535037dcf06da3ecc7adc553
Gustav Karlsson <gustav.karlsson@gmail.com>
2022-07-30 12:00:34 +0200

    Downgrade ktlint again!

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -12,7 +12,7 @@ object Versions {
 
     const val playPublisher = "3.7.0"
     const val ktlintGradle = "10.3.0"
-    const val ktlint = "0.45.1" // Skip 0.45.2 as it has a bug: https://github.com/pinterest/ktlint/pull/1450
+    const val ktlint = "0.44.0" // Skip 0.45.x as it has a bug: https://github.com/pinterest/ktlint/pull/1450
 
     // App
     // Google
