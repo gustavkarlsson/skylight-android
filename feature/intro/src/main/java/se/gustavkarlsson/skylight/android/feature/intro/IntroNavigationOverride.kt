336aacb75f52447846f676d3cbc9a215b0d30b1d
Gustav Karlsson <gustav.karlsson@gmail.com>
2022-10-21 22:29:27 +0200

    Remove useless call to runBlocking

--- feature/intro/src/main/java/se/gustavkarlsson/skylight/android/feature/intro/IntroNavigationOverride.kt
+++ feature/intro/src/main/java/se/gustavkarlsson/skylight/android/feature/intro/IntroNavigationOverride.kt
@@ -1,8 +1,5 @@
 package se.gustavkarlsson.skylight.android.feature.intro
 
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.runBlocking
-import se.gustavkarlsson.skylight.android.core.Io
 import se.gustavkarlsson.skylight.android.lib.navigation.Backstack
 import se.gustavkarlsson.skylight.android.lib.navigation.NavigationOverride
 import se.gustavkarlsson.skylight.android.lib.navigation.Screen
@@ -10,17 +7,15 @@ import se.gustavkarlsson.skylight.android.lib.runversion.RunVersionManager
 import javax.inject.Inject
 
 internal class IntroNavigationOverride @Inject constructor(
-    @Io private val dispatcher: CoroutineDispatcher,
     private val runVersionManager: RunVersionManager,
 ) : NavigationOverride {
     override val priority = 10
 
-    override fun override(oldBackstack: Backstack, targetBackstack: Backstack): Backstack? =
-        runBlocking(dispatcher) { // TODO Do we have to run blocking?
-            when {
-                targetBackstack.screens.none { it.type == Screen.Type.Intro } &&
-                    runVersionManager.isFirstRun -> Backstack(IntroScreen(targetBackstack))
-                else -> null
-            }
+    override fun override(oldBackstack: Backstack, targetBackstack: Backstack): Backstack? {
+        val introScreenOnStack = targetBackstack.screens.any { it.type == Screen.Type.Intro }
+        return when {
+            !introScreenOnStack && runVersionManager.isFirstRun -> Backstack(IntroScreen(targetBackstack))
+            else -> null
         }
+    }
 }
