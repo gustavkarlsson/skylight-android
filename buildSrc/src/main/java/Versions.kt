ff25f869a54881304f815dea55c6a1196222f474
Gustav Karlsson <gustav.karlsson@gmail.com>
2023-08-25 22:40:59 +0200

    Use version catalogs in buildSrc

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -5,6 +5,5 @@ object Versions {
     const val compileSdk = 34
     val java = JavaVersion.VERSION_1_8
     const val composeCompiler = "1.5.1"
-    const val jgit = "5.12.0.202106070339-r"
     const val ktlint = "0.50.0"
 }
